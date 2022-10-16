bde6d64bbeff3e6a4462c5c2471fb09aed930638
Zien Alhaoushe <zienalhaoushe@hotmail.com>
2022-10-16 19:47:02 +0300

    singular/plurar case in unit converter UI

--- solo-unit-converter/index.js
+++ solo-unit-converter/index.js
@@ -20,28 +20,70 @@ function render(val) {
   kilogramCalc(val);
 }
 //meter calculation
+function singularPlural(num, name1, name2) {
+  if (num > 1) {
+    return name1;
+  } else {
+    return name2;
+  }
+}
 function meterCalc(inputVal) {
-  console.log(inputVal);
-  let string = `${inputVal} meters = ${(inputVal * 3.281).toFixed(
-    3
-  )} feet | ${inputVal}  feet = ${(inputVal / 3.281).toFixed(3)} meters`;
-  console.log(string);
+  let meterVal = (inputVal * 3.281).toFixed(3);
+  let meterVal2 = (inputVal / 3.281).toFixed(3);
+
+  let string = `${inputVal} ${singularPlural(
+    inputVal,
+    "meters",
+    "meter"
+  )} =${meterVal} ${singularPlural(
+    meterVal,
+    "feet",
+    "foot"
+  )} | ${inputVal} ${singularPlural(
+    inputVal,
+    "feet",
+    "foot"
+  )} = ${meterVal2} ${singularPlural(meterVal2, "meters", "meter")} `;
   meterEl.textContent = string;
 }
 //liter calculation
 
 function literCalc(inputVal) {
-  let string = `${inputVal} liters = ${(inputVal * 0.264).toFixed(
-    3
-  )} gallons | ${inputVal}  gallons  = ${(inputVal / 0.264).toFixed(3)} liters`;
+  let litreVal1 = (inputVal * 0.264).toFixed(3);
+  let litreVal2 = (inputVal / 0.264).toFixed(3);
+  let string = `${inputVal} ${singularPlural(
+    inputVal,
+    "liters",
+    "liter"
+  )} = ${litreVal1}  ${singularPlural(
+    inputVal,
+    "gallons",
+    "gallon"
+  )}  | ${inputVal}   ${singularPlural(
+    litreVal1,
+    "gallons",
+    "gallon"
+  )}  = ${litreVal2}  ${singularPlural(litreVal2, "liters", "liter")}`;
   literEl.textContent = string;
 }
 //kilogram calculation
 
 function kilogramCalc(inputVal) {
-  let string = `${inputVal} kilos  = ${(inputVal * 2.204).toFixed(
-    3
-  )} pounds  | ${inputVal}  pounds   = ${(inputVal / 2.204).toFixed(3)} kilos `;
+  let kiloVal1 = (inputVal * 2.204).toFixed(3);
+  let kiloVal2 = (inputVal / 2.204).toFixed(3);
+  let string = `${inputVal} ${singularPlural(
+    inputVal,
+    "kilos",
+    "kilo"
+  )}= ${kiloVal1} ${singularPlural(
+    kiloVal1,
+    "pounds",
+    "pound"
+  )}    | ${inputVal} ${singularPlural(
+    inputVal,
+    "pounds",
+    "pound"
+  )}      = ${kiloVal2} ${singularPlural(kiloVal2, "kilos", "kilo")} `;
   kiloEl.textContent = string;
 }
 
