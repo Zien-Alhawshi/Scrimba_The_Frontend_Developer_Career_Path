2c6a88a0eea38356cb89de286ba2198a7f11ea6a
Zien <zienalhaoushe@hotmail.com>
2022-12-06 16:34:53 +0200

    solo-movie list app done!

--- solo-movie-watch-list/index.js
+++ solo-movie-watch-list/index.js
@@ -7,63 +7,108 @@ let allFilms = [];
 let result = [];
 let html = "";
 let contentPlaceHolder = document.getElementById("content");
-form.addEventListener("submit", function (e) {
+
+document.addEventListener("submit", function (e) {
+  html = "";
+
   allFilms = [];
   fetch(`http://www.omdbapi.com/?apikey=4d8a6d37&s=${filmName.value}`)
-    .then((res) => res.json())
+   .then(res=>{
+     if(res.ok){
+      return  res.json()
+     }
+     throw new Error();
+
+
+   })
     .then((data) => {
-      films = data.Search;
-      filmsID = films.map((item) => {
-        return item.imdbID;
+      console.log(data)
+        films = data.Search;
+        filmsID = films.map((item) => {
+          return item.imdbID;
+        })
+        renderFilms(filmsID)
+      })
+      .catch(() => {
+        contentPlaceHolder.innerHTML = `
+        <div class="watchlist-content">
+          <p class="lighter ligther-search">Unable to find what you’re looking for. Please try another search.</p>
+        </div>
+
+        `;
       });
+      filmName.value = ""
+      e.preventDefault();
 
-      filmsID.forEach((element) => {
-        fetch(`http://www.omdbapi.com/?apikey=4d8a6d37&i=${element}`)
-          .then((res) => res.json())
-          .then((data) => {
-            if (data) {
-              console.log(data);
-              allFilms.push(data);
+  
+    })
+ 
+  
+function renderFilms(filmsID){
+  filmsID.forEach((element) => {
+    fetch(`http://www.omdbapi.com/?apikey=4d8a6d37&i=${element}`)
 
-              html += `
-              <div class="movie">
-                  <img class="poster" src="${data.Poster}">
-                  <div class"movie-data">
-                      <h3 >${data.Title}     <i class="fas fa-star star"></i> ${data.imdbRating}</h3>
-                      <div class="movies-info">
-                          <p>${data.Runtime}</p>
-                          <p>  ${data.Genre}</p>
-                          <p> <i class="fas fa-plus-circle"></i> <a id="${data.imdbID}" class="watchlist-add">Watchlist</a></p>
-                      </div>
-                    </p>
-                      <p class="plot">${data.Plot}</p>
+      .then((res) =>res.json())
+      .then((data) => {
+        
+          console.log(data);
+          allFilms.push(data);
+
+          html += `
+          <div class="movie">
+              <img class="poster" src="${data.Poster}">
+              <div class"movie-data">
+                  <h3 >${data.Title}     <i class="fas fa-star star"></i> ${data.imdbRating}</h3>
+                  <div class="movies-info">
+                      <p>${data.Runtime}</p>
+                      <p>  ${data.Genre}</p>
+                      <p id="">
+                      <a id="${data.imdbID}" class="watchlist-add"> <i class="fas fa-plus-circle"></i> Watchlist</a></p>
                   </div>
+                </p>
+                  <p class="plot">${data.Plot}</p>
               </div>
-              `;
-            } else {
-              html = `
-              No data
-              `;
-            }
+          </div>
+          `;
+        
+        contentPlaceHolder.innerHTML = html;
 
-            // console.log(html);
-            contentPlaceHolder.innerHTML = html;
-          });
-      });
-    });
-  e.preventDefault();
-});
+
+      })
+      
+    
+
+  });
+}
+
+
+
 document.addEventListener("click", function (e) {
-  console.log(allFilms);
-  console.log(e.target.id);
-  // console.log(document.querySelector(".watching").dataset.movieID);
-  // allFilms.forEach((element) => {
-  //   //detect which movie is added to the watch list
-  //   // console.log();
-  //   // if(element.imdbID ==  ){
-  //   //   selectedMovies.push(element)
-  //   // }
-  // });
+
+  allFilms.forEach((element) => {
+    if(element.imdbID == e.target.id ){
+      if(!selectedMovies.includes(element)){
+        storeFilms(element)
+
+      }
+      document.getElementById(`${element.imdbID}`).innerHTML = `<i class="fa fa-check-circle" aria-hidden="true"></i>Added</a>`;
+      document.getElementById(`${element.imdbID}`).style.disabled = "true"
+
+    }
+
+  });
+
 });
 
-//in watch list we render the movies available in selectedMovies
+
+
+function storeFilms(newFilm) {
+  let filmsData;
+  if (localStorage.getItem("filmsData") === null) {
+    filmsData = [];
+  } else {
+    filmsData = JSON.parse(localStorage.getItem("filmsData"));
+  }
+  filmsData.push(newFilm);
+  localStorage.setItem("filmsData", JSON.stringify(filmsData));
+}
---
+++ solo-movie-watch-list/index2.js
@@ -0,0 +1,75 @@
+let htmlContent = ""
+
+ let selected = getFilms()
+renderLocalStorage(selected)
+console.log(selected)
+
+document.addEventListener("click", function(e){
+    console.log(e.target.id)
+    selected.forEach((element) => {
+    if(element.imdbID == e.target.id ){
+        const indexOfObject = selected.findIndex(object => {
+            return object.imdbID ===  e.target.id;
+          });
+          console.log(indexOfObject)
+          selected.splice(indexOfObject, 1);
+          localStorage.setItem("filmsData", JSON.stringify(selected));
+          
+
+    }
+    console.log(selected)
+    if(selected.length >0){
+        renderLocalStorage(selected)
+    }
+    else{
+        htmlContent = `
+        <p class="lighter">Your watchlist is looking a little empty...</p>
+        <p><i class="fas fa-plus-circle"></i> <a href="index.html"> Let's add some movies!</a></p>
+        ` 
+        render(htmlContent)
+    }
+    
+
+e.preventDefault()
+  });
+  
+
+
+})
+
+function getFilms() {
+    let filmsData;
+    if (localStorage.getItem("filmsData") === null) {
+      window.localStorage.setItem("filmsData", JSON.stringify(filmsData));
+    } else {
+        filmsData = JSON.parse(localStorage.getItem("filmsData"));
+    }
+    return filmsData;
+  }
+  function renderLocalStorage(selected){
+    htmlContent=""
+    selected.forEach(data => {
+        htmlContent += `
+        <div class="movie">
+        <img class="poster" src="${data.Poster}">
+        <div class"movie-data">
+            <h3 >${data.Title}     <i class="fas fa-star star"></i> ${data.imdbRating}</h3>
+            <div class="movies-info">
+                <p>${data.Runtime}</p>
+                <p>  ${data.Genre}</p>
+                <p id="">
+                <a id="${data.imdbID}" class="watchlist-add"> <i class="fas fa-minus-circle"></i> Remove</a></p>
+            </div>
+          </p>
+            <p class="plot">${data.Plot}</p>
+        </div>
+    </div>
+        `
+        
+    });
+    render(htmlContent)   
+}
+function render(htmlContent){
+    document.getElementById("localStorage").innerHTML = htmlContent
+
+}
