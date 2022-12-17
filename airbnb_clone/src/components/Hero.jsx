9a8823c1e8611b891ba4c4660f5560b025fb2c15
Zien <zienalhaoushe@hotmail.com>
2022-12-17 19:02:42 +0200

    org

--- airbnb_clone/src/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-
-import grid from "../assets/images/photo-grid.png"
-export default function Hero(){
-    return(
-        <div className="grid">
-            <img src={grid} className="grid--img"/>
-        </div>
-    )
-}
