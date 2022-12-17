38699be8d007d8c9bfd7a6fdc289bf503dd1d146
Zien <zienalhaoushe@hotmail.com>
2022-12-17 19:05:26 +0200

    airbnb project

---
+++ airbnb_clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+import img from "../assets/images/airbnb 1.png"
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <img src ={img}  className="navbar--img" />
+        </div>
+    )
+}
